f123659ce29ab050b2c8215243431a7b6965f4f8
MikMickey <name@provider.com>
2021-06-25 08:52:00 -0700

    deliverable 2 challenge

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -12,5 +12,15 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 #Summary
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data= MechaCar))
 
+#Deliveranle 2
+
+#read file
+Suspension <- read.csv(file='Suspension_coil.csv',check.names=F,stringsAsFactors = F)
+
+#create total summary
+summarize_sc <- Suspension %>% summarize(Mean=mean(PSI),Median=(PSI),Variance=var(PSI),SD=sd(PSI)) 
+
+#create a lot summary
+lot_sc <- Suspension %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=(PSI),Variance=var(PSI),SD=sd(PSI))
 
 
